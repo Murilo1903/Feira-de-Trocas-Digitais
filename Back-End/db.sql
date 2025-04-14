65ffad7b5bfc438a62698daf9c6a6c1618bc2386
Murilo1903 <murilofb1983@gmail.com>
2025-04-14 09:25:41 -0300

    criei o banco

---
+++ Back-End/db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE Feira;
+USE Feira;
+
+CREATE TABLE Usuarios (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(35) NOT NULL,
+    senha VARCHAR(15) NOT NULL,
+    email VARCHAR(35) UNIQUE NOT NULL,
+    reputacao INT NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Postagens (
+	id_postagem INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    titulo VARCHAR(40) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    curtidas INT NOT NULL,
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario),
+	criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Comentarios(
+	id_comentario INT AUTO_INCREMENT PRIMARY KEY,
+	id_postagem INT NOT NULL,
+    id_usuario INT NOT NULL,
+	FOREIGN KEY (id_postagem) REFERENCES Postagens(id_postagem),
+	FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
+);
